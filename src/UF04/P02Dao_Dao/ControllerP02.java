e75f445d5c58f4a2a7a6078d7dab693473b06aa9
47257165p <47257165p@iespoblenou.org>
2016-04-19 17:43:11 +0200

    iniciando el jaxb

--- src/UF04/P02Dao_Dao/ControllerP02.java
+++ src/UF04/P02Dao_Dao/ControllerP02.java
@@ -1,12 +1,27 @@
 package UF04.P02Dao_Dao;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import java.io.File;
+
 /**
  * Created by 47257165p on 19/04/16.
  */
 public class ControllerP02 {
 
-    public static void main(String[] args) {
+    private static DataType database;
+    private static JAXBContext context;
+    private static File file = new File("UF04/P02Dao_Dao/data.xml");
+
+    public static void main(String[] args) throws JAXBException {
 
         DAO dao = new DAO("localhost", "8080", "admin", "admin");
+
+        Marshaller MS = context.createMarshaller();
+        MS.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        MS.marshal(database, file);
+        System.out.println("\nProducto añadido");
     }
 }
